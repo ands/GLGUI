a1e2f59be5dca9cfd2e94dfd5c78b86ab9dd4672
ands <ands>
2014-04-10 00:55:02 +0200

    updated OpenTK.dll to 73525e3f23560b4b7c95198c6ff2549292c89342 some example code cleanup

--- GLGUI/GLGUI.Example/MainForm.cs
+++ GLGUI/GLGUI.Example/MainForm.cs
@@ -1,56 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
-using System.Text;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
 namespace GLGUI.Example
 {
-    public class LineWriter : TextWriter
-    {
-        public List<string> Lines;
-        public bool Changed = false;
-        private StringBuilder currentLine;
-
-        public LineWriter()
-        {
-            this.Lines = new List<string>(1024);
-            this.currentLine = new StringBuilder(256);
-        }
-
-        public override void Write(char value)
-        {
-            if (value == '\n')
-                Flush();
-            else
-                currentLine.Append(value);
-        }
-
-        public override void Flush()
-        {
-            Lines.Add(currentLine.ToString());
-            if (Lines.Count > 1024)
-                Lines.RemoveAt(0);
-            currentLine.Clear();
-            Changed = true;
-        }
-
-        public override Encoding Encoding
-        {
-            get { return System.Text.Encoding.UTF8; }
-        }
-
-        public void Clear()
-        {
-            Lines.Clear();
-            currentLine.Clear();
-            Changed = true;
-        }
-    }
-
 	public class MainForm : GameWindow
 	{
 		GLGui glgui;
@@ -58,24 +13,29 @@ namespace GLGUI.Example
         GLLabel console;
         LineWriter consoleWriter;
 
+        int fpsCounter = 0;
+        int fpsSecond = 1;
+        double time = 0.0;
+
 		public MainForm() : base(1024, 600, new GraphicsMode(new ColorFormat(8, 8, 8, 8), 24, 0, 4), "GLGUI Example")
 		{
             consoleWriter = new LineWriter();
             Console.SetOut(consoleWriter);
             Console.SetError(consoleWriter);
 
-			this.Load += OpenGLLoad;
+			this.Load += OnLoad;
 			this.Resize += (s, e) => GL.Viewport(ClientSize);
-			this.RenderFrame += OpenGLFrame;
+			this.RenderFrame += OnRender;
 		}
 
-		private void OpenGLLoad(object src, EventArgs ev)
+        private void OnLoad(object sender, EventArgs e)
 		{
-			VSync = VSyncMode.Off;
+			VSync = VSyncMode.Off; // vsync is nice, but you can't really measure performance while it's on
 
 			glgui = new GLGui(this);
             
-            var mainAreaControl = glgui.Add(new GLGroupLayout(glgui) { Outer = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height - 200), Anchor = GLAnchorStyles.Left | GLAnchorStyles.Right | GLAnchorStyles.Top | GLAnchorStyles.Bottom });
+            var mainAreaControl = glgui.Add(new GLGroupLayout(glgui) { Size = new Size(ClientSize.Width, ClientSize.Height - 200), Anchor = GLAnchorStyles.All });
+            // change background color:
             var mainSkin = mainAreaControl.Skin;
 			mainSkin.BackgroundColor = glgui.Skin.FormActive.BackgroundColor;
 			mainSkin.BorderColor = glgui.Skin.FormActive.BorderColor;
@@ -83,120 +43,60 @@ namespace GLGUI.Example
 
             var consoleScrollControl = glgui.Add(new GLScrollableControl(glgui) { Outer = new Rectangle(0, ClientSize.Height - 200, ClientSize.Width, 200), Anchor = GLAnchorStyles.Left | GLAnchorStyles.Right | GLAnchorStyles.Bottom });
             console = consoleScrollControl.Add(new GLLabel(glgui) { AutoSize = true });
-            var skin = console.SkinEnabled;
-			skin.Font = new GLFont(new Font("Arial", 8.0f));
-			skin.BackgroundColor = glgui.Skin.TextBoxActive.BackgroundColor;
-            console.SkinEnabled = skin;
 
             fpsLabel = mainAreaControl.Add(new GLLabel(glgui) { Location = new Point(10, 10), AutoSize = true });
+            // change font and background color:
+            var skin = fpsLabel.SkinEnabled;
+            skin.Font = new GLFont(new Font("Arial", 12.0f));
+            skin.BackgroundColor = glgui.Skin.TextBoxActive.BackgroundColor;
             fpsLabel.SkinEnabled = skin;
 
-            var a = mainAreaControl.Add(new GLForm(glgui) { Title = "Hello World", Outer = new Rectangle(100, 100, 200, 150), AutoSize = false });
-            var c = a.Add(new GLForm(glgui) { Title = "Hello Form", Outer = new Rectangle(10, 10, 100, 100), AutoSize = false });
-			c.MouseMove += (s, e) => Console.WriteLine(e.Position.ToString());
+            var helloWorldForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Hello World", Outer = new Rectangle(50, 100, 200, 150), AutoSize = false });
+            helloWorldForm.Add(new GLForm(glgui) { Title = "Hello Form", Outer = new Rectangle(100, 32, 100, 100), AutoSize = false })
+                .MouseMove += (s, w) => Console.WriteLine(w.Position.ToString());
 
-            var flow = a.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.BottomUp, Outer = new Rectangle(0, 0, a.Inner.Width, a.Inner.Height), AutoSize = true });
+            var flow = helloWorldForm.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.BottomUp, Location = new Point(10, 10), Size = helloWorldForm.InnerSize, AutoSize = true });
             for (int i = 0; i < 5; i++)
-                flow.Add(new GLButton(glgui) { Text = "Button" + i, Outer = new Rectangle(10, 32, 150, 0) }).Click += (s, e) => Console.WriteLine(s + " pressed.");
-			flow.Add(new GLButton(glgui) { Text = "Hide Cursor", Outer = new Rectangle(10, 32, 150, 0) }).Click += (s, e) => glgui.Cursor = GLCursor.None;
+                flow.Add(new GLButton(glgui) { Text = "Button" + i, Outer = new Rectangle(10, 32, 150, 0) })
+                    .Click += (s, w) => Console.WriteLine(s + " pressed.");
+			flow.Add(new GLButton(glgui) { Text = "Hide Cursor", Outer = new Rectangle(10, 32, 150, 0) })
+                .Click += (s, w) => glgui.Cursor = GLCursor.None;
 
-			for (int i = 0; i < 1; i++)
+            var loremIpsumForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Lorem Ipsum", Location = new Point(600, 100), Size = new Size(300, 200) });
+            loremIpsumForm.Add(new GLTextBox(glgui) {
+                Text = "Lorem ipsum dolor sit amet,\nconsetetur sadipscing elitr,\nsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,\nsed diam voluptua.\n\nAt vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                Multiline = true, 
+                WordWrap = true, 
+                Outer = new Rectangle(4, 4, loremIpsumForm.Inner.Width - 8, loremIpsumForm.Inner.Height - 8), 
+                Anchor = GLAnchorStyles.All 
+            }).Changed += (s, w) => Console.WriteLine(s + " text length: " + ((GLTextBox)s).Text.Length);
+
+            var fixedSizeForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Fixed size Form", Location = new Point(64, 300), Size = new Size(100, 200), AutoSize = true });
+            var fooBarFlow = fixedSizeForm.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.TopDown, AutoSize = true });
+            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "CheckBox A", AutoSize = true })
+                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
+            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "CheckBox B", AutoSize = true })
+                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
+            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "Totally different CheckBox", AutoSize = true })
+                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
+            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "Go away!", AutoSize = true })
+                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
+
+			for (int i = 0; i < 3; i++)
 			{
-                var b = mainAreaControl.Add(new GLForm(glgui) { Title = "Cube", Size = new Size(200, 200) });
-                //var c = b.Add(new GLScrollableControl(glgui) { Outer = new Rectangle(0, 0, b.Inner.Width, b.Inner.Height), ContentSize = new Size(400, 200), Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom });
-                var d = b.Add(new GLViewport(glgui) { Size = b.InnerSize, Anchor = GLAnchorStyles.Left | GLAnchorStyles.Top | GLAnchorStyles.Right | GLAnchorStyles.Bottom });
-                double t = 0.0f;
-                d.RenderViewport += (vp, delta) =>
-                {
-                    GL.Enable(EnableCap.DepthTest);
-                    GL.ClearColor(0, 0, 0, 1);
-                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-                    GL.MatrixMode(MatrixMode.Projection);
-                    var proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90.0f), d.AspectRatio, 1.0f, 100.0f);
-                    GL.LoadMatrix(ref proj);
-                    GL.MatrixMode(MatrixMode.Modelview);
-                    GL.LoadIdentity();
-
-                    GL.Translate(0, 0, -2.0f);
-                    GL.Rotate(t * 100.0f, 1, 0, 0);
-                    GL.Rotate(t * 42.0f, 0, 1, 0);
-                    t += delta;
-
-					GL.Begin(PrimitiveType.Quads);
-                    GL.Color3(1.0, 0.0, 0.0);
-                    GL.Vertex3(0.5, -0.5, -0.5);
-                    GL.Color3(0.0, 1.0, 0.0);
-                    GL.Vertex3(0.5, 0.5, -0.5);
-                    GL.Color3(0.0, 0.0, 1.0);
-                    GL.Vertex3(-0.5, 0.5, -0.5);
-                    GL.Color3(1.0, 0.0, 1.0);
-                    GL.Vertex3(-0.5, -0.5, -0.5);
-
-                    GL.Color3(1.0, 1.0, 1.0);
-                    GL.Vertex3(0.5, -0.5, 0.5);
-                    GL.Vertex3(0.5, 0.5, 0.5);
-                    GL.Vertex3(-0.5, 0.5, 0.5);
-                    GL.Vertex3(-0.5, -0.5, 0.5);
-
-                    GL.Color3(1.0, 0.0, 1.0);
-                    GL.Vertex3(0.5, -0.5, -0.5);
-                    GL.Vertex3(0.5, 0.5, -0.5);
-                    GL.Vertex3(0.5, 0.5, 0.5);
-                    GL.Vertex3(0.5, -0.5, 0.5);
-
-                    GL.Color3(0.0, 1.0, 0.0);
-                    GL.Vertex3(-0.5, -0.5, 0.5);
-                    GL.Vertex3(-0.5, 0.5, 0.5);
-                    GL.Vertex3(-0.5, 0.5, -0.5);
-                    GL.Vertex3(-0.5, -0.5, -0.5);
-
-                    GL.Color3(0.0, 0.0, 1.0);
-                    GL.Vertex3(0.5, 0.5, 0.5);
-                    GL.Vertex3(0.5, 0.5, -0.5);
-                    GL.Vertex3(-0.5, 0.5, -0.5);
-                    GL.Vertex3(-0.5, 0.5, 0.5);
-
-                    GL.Color3(1.0, 0.0, 0.0);
-                    GL.Vertex3(0.5, -0.5, -0.5);
-                    GL.Vertex3(0.5, -0.5, 0.5);
-                    GL.Vertex3(-0.5, -0.5, 0.5);
-                    GL.Vertex3(-0.5, -0.5, -0.5);
-                    GL.End();
-
-                    GL.Disable(EnableCap.DepthTest);
-                };
-
-                b = mainAreaControl.Add(new GLForm(glgui) { Title = "Lorem Ipsum", Size = new Size(300, 200) });
-				var x = b.Add(new GLTextBox(glgui) { Text = "Lorem ipsum dolor sit amet,\nconsetetur sadipscing elitr,\nsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,\nsed diam voluptua.\n\nAt vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", Multiline = true, WordWrap = true, Outer = new Rectangle(4, 4, b.Inner.Width - 8, b.Inner.Height - 8), Anchor = GLAnchorStyles.Left | GLAnchorStyles.Top | GLAnchorStyles.Right | GLAnchorStyles.Bottom });
-                x.Changed += (z, e) => Console.WriteLine(z + " text length: " + ((GLTextBox)z).Text.Length);
-
-                b = mainAreaControl.Add(new GLForm(glgui) { Title = "Foo Bar", Size = new Size(100, 200), AutoSize = true });
-				//b.Add(new GLButton(glgui) { Text = "Foo Bar", Outer = new Rectangle(32, 24, 75, 50), Anchor = AnchorStyles.Right, AutoSize = true });
-                flow = b.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.TopDown, AutoSize = true });
-				/*var s = flow.Skin;
-				s.BackgroundColor = Color.FromArgb(255, 255, 255);
-				flow.Skin = s;*/
-                flow.Add(new GLCheckBox(glgui) { Text = "Trololo lolo", AutoSize = true }).Changed += (z, e) => Console.WriteLine(z + ": " + ((GLCheckBox)z).Checked);
-                flow.Add(new GLCheckBox(glgui) { Text = "Troll", AutoSize = true }).Changed += (z, e) => Console.WriteLine(z + ": " + ((GLCheckBox)z).Checked);
-                flow.Add(new GLCheckBox(glgui) { Text = "lolololo", AutoSize = true }).Changed += (z, e) => Console.WriteLine(z + ": " + ((GLCheckBox)z).Checked);
-				flow.Add(new GLCheckBox(glgui) { Text = "Trololo lolo", AutoSize = true }).Changed += (z, e) => Console.WriteLine(z + ": " + ((GLCheckBox)z).Checked);
+                var viewportForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Cube", Location = new Point(300 + i * 16, 64 + i * 16), Size = new Size(200, 200) });
+                viewportForm.Add(new GLViewport(glgui) { Size = viewportForm.InnerSize, Anchor = GLAnchorStyles.All })
+                    .RenderViewport += OnRenderViewport;
 			}
 		}
 
-		int fpsCounter = 0;
-		int fpsSecond = 1;
-		double time = 0.0;
-
-		private void OpenGLFrame(object s, FrameEventArgs e)
+        private void OnRender(object sender, FrameEventArgs e)
 		{
 			time += e.Time;
 
 			if (time >= fpsSecond)
 			{
-				fpsLabel.Text = "Application: " + fpsCounter.ToString("0") +
-				                "FPS. GLGUI: " + glgui.RenderDuration.ToString("0.0") +
-				                "ms (" + (1000.0 / glgui.RenderDuration).ToString("0") + "FPS)";
+				fpsLabel.Text = string.Format("Application: {0:0}FPS. GLGUI: {1:0.0}ms", fpsCounter, glgui.RenderDuration);
 				fpsCounter = 0;
 				fpsSecond++;
 			}
@@ -212,6 +112,69 @@ namespace GLGUI.Example
 
 			fpsCounter++;
 		}
+
+        // draws simple colored cube in a GLViewport control
+        private void OnRenderViewport(object sender, double deltaTime)
+        {
+            var viewport = (GLViewport)sender;
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(0, 0, 0, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            var proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90.0f), viewport.AspectRatio, 1.0f, 100.0f);
+            GL.LoadMatrix(ref proj);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Translate(0, 0, -2.0f);
+            GL.Rotate(time * 100.0f, 1, 0, 0);
+            GL.Rotate(time * 42.0f, 0, 1, 0);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color3(1.0, 0.0, 0.0);
+            GL.Vertex3(0.5, -0.5, -0.5);
+            GL.Color3(0.0, 1.0, 0.0);
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Color3(0.0, 0.0, 1.0);
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Color3(1.0, 0.0, 1.0);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+
+            GL.Color3(1.0, 1.0, 1.0);
+            GL.Vertex3(0.5, -0.5, 0.5);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+
+            GL.Color3(1.0, 0.0, 1.0);
+            GL.Vertex3(0.5, -0.5, -0.5);
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(0.5, -0.5, 0.5);
+
+            GL.Color3(0.0, 1.0, 0.0);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+            GL.Vertex3(-0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+
+            GL.Color3(0.0, 0.0, 1.0);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Vertex3(-0.5, 0.5, 0.5);
+
+            GL.Color3(1.0, 0.0, 0.0);
+            GL.Vertex3(0.5, -0.5, -0.5);
+            GL.Vertex3(0.5, -0.5, 0.5);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+            GL.End();
+
+            GL.Disable(EnableCap.DepthTest);
+        }
 	}
 }
 
